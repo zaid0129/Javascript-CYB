69eb0f9d9e82a7ab90bc5c24358cc0210e3a9ae5
Zaid Khan <zaidkhann0129@gmail.com>
2025-01-23 11:44:41 +0530

    commit

---
+++ arrayHW.js
@@ -0,0 +1,47 @@
+function zaid (){
+
+
+
+    let sports=["cricket", "football", "volleyball", "tennis"]  //ARRAY
+    
+    
+    
+    //-------------------LENGTH------------------//
+    
+    // document.write(sports.length)  //return length
+    
+    document.write(sports[3])   //access
+    
+    
+    
+    //----------------------POP-----------------------//
+    
+    // sports.pop()   //tennis remove  // REMOVE LAST ELEMENT
+    
+    // document.write(sports)  //TO PRINT ARRAY 
+    
+    
+    
+    //------------------------SHIFT---------------------//
+    
+    // sports.shift()  //cricket remove //REMOVE 1ST ELEMENT 
+    
+    // document.write(sports)  //TO PRINT ARRAY
+    
+    
+    
+    //--------------------UNSHIFT---------------------//
+    
+    // sports.unshift("BASKETBALL")   //TO ADD NEW ELEMENTS FROM FIRST
+    
+    // document.write(sports)     // TO PRINT ARRAY
+    
+    
+    //--------------------PUSH-----------------------//
+    
+    // sports.push("RUGBY")  //TO ADD NEW ELEMENTS FROM LAST
+    
+    // document.write(sports)  //TO PRINT ARRAY
+    
+    
+    }
